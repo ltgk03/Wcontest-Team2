301993f1bd4a3a7a5f6f16f30e7e23d2ac9a9799
ltgk03 <21020476@vnu.edu.vn>
2022-10-09 14:43:36 +0700

    admin login function

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const mysql = require('mysql');
+const helper = require('./helper');
 const app = express();
 
-const port = 3000;
+var port = 3000;
 var isAdmin = false;
 
 //Static Files
@@ -16,6 +18,15 @@ app.use('img', express.static(__dirname + 'public/img'));
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// Connect database
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'my_db'
+});
+
 
 // API
 app.get('/', (req, res) => {
@@ -23,19 +34,34 @@ app.get('/', (req, res) => {
 });
 
 app.get('/admin', (req, res) => {
-    if (isAdmin) {res.render('adminUI')}
-    else {
+    if (isAdmin) {
+        res.render('adminUI');
+    } else {
         res.send("You don't have permission!");
     }
-});
+})
     
 app.get('/login', (req, res) => {
-    res.render('adminLogin');
+    const protocol = req.protocol;
+    const host = req.hostname;
+    const reqUrl = req.originalUrl;
+    const url = new URL(`${protocol}://${host}:${port}${reqUrl}`);
+    if (Array.from(url.searchParams).length == 0) {
+        res.render('adminLogin');
+    } else {
+        const acc = url.searchParams.get('admin-acc');
+        const pass = url.searchParams.get('admin-password');
+        const sql = `SELECT * FROM adminAccounts WHERE username='${acc}' AND userpassword='${pass}'`; 
+        connection.query(sql, function (err, result) {
+            if (err) throw err;
+            if (result.length > 0) {
+                isAdmin = true;
+                res.redirect('/admin');
+            } else {
+                res.redirect('/login');
+            }
+        });
+    }
 });
 
 app.listen(port, () => console.info(`Listening on port: ${port}`));
-
-
-
-
-
