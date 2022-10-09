72eb439887398802d2b83c21f7cfdd19882367ae
Hoang Ancient <hoangdinhnho23@gmail.com>
2022-10-09 22:14:37 +0700

    admin create
    a

--- config.php
+++ config.php
@@ -1,5 +1,10 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "qrabiloo";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed" . $conn->connect_error);
+}
