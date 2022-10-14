3f6511396d428174620182ae02f025e15790302e
duyvu1110 <100179213+duyvu1110@users.noreply.github.com>
2022-10-14 14:57:48 +0700

    commit lại xóa trùng lặp ở update

--- update.php
+++ update.php
@@ -14,86 +14,12 @@ include "config.php";
 
 //     $result = $conn->query($sql);
 
-    if ($result == TRUE) {
-        echo "<p>Record Updated Succesfully!</p>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-if (isset($_GET['id'])) {
-    $question_id = $_GET['id'];
-
-    $sql = "SELECT *FROM question WHERE id = '$question_id'";
-
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $quest = $row['quest'];
-            $question_id = $row['id'];
-            $answer1 = $row['answer1'];
-            $answer2 = $row['answer2'];
-            $answer3 = $row['answer3'];
-            $answer4 = $row['answer4'];
-            $ranswer = $row['ranswer'];
-        }
-
-?>
-        <!DOCTYPE html>
-        <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <link rel="stylesheet" href="./assets/css/update.css">
-        </head>
-
-        <body>
-            <h2 class = "update_form">Update form</h2>
-            <form action="" method="POST">
-                <fieldset class = "Fieldset">
-                    <legend class = "Legend" >Update Question</legend>
-                    <div class = "float container">
-                        <div class = "float-child">
-                    Question:<br>
-                    <input type="text" name="quest">
-    </div>
-                    <br>
-                    <div class = "float-child">
-                    Answer1:<br>
-                    <input type="text" name="answer1">
-                    <br>
-                    Answer2:<br>
-                    <input type="text" name="answer2">
-                    <br>
-                    Answer3:<br>
-                    <input type="text" name="answer3">
-                    <br>
-                    Answer4:<br>
-                    <input type="text" name="answer4">
-    
-                    <br><?php
-include "config.php";
-
-if (isset($_POST['update'])) {
-    $quest = $_POST['quest'];
-    $question_id = $_GET['id'];
-    $answer1 = $_POST['answer1'];
-    $answer2 = $_POST['answer2'];
-    $answer3 = $_POST['answer3'];
-    $answer4 = $_POST['answer4'];
-    $ranswer = $_POST['ranswer'];
-
-    $sql = "UPDATE question SET quest = '$quest', answer1 = '$answer1', answer2 = '$answer2', answer3 = '$answer3', answer4 = '$answer4',  ranswer = '$ranswer' WHERE id = '$question_id'";
-
-    $result = $conn->query($sql);
-
-    if ($result == TRUE) {
-        echo "<p>Record Updated Succesfully!</p>";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-}
+//     if ($result == TRUE) {
+//         echo "<p>Record Updated Succesfully!</p>";
+//     } else {
+//         echo "Error: " . $sql . "<br>" . $conn->error;
+//     }
+// }
 if (isset($_GET['id'])) {
     $question_id = $_GET['id'];
 
@@ -192,5 +118,5 @@ if (isset($_GET['id'])) {
     }
     }
 }
-
+    
 ?>
