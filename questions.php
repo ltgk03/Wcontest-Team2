0f83a8755d36e780b54fdd949520c6796a48bf7f
ltgk03 <21020476@vnu.edu.vn>
2022-10-16 02:40:46 +0700

    ltgk03: 16/10/2022

--- questions.php
+++ questions.php
@@ -13,35 +13,46 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
     <!-- Optional theme -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
     <!-- Latest compiled and minified JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"></script>
     <link rel="stylesheet" href="./assets/css/questions.css">
 </head>
 <body>
-    <!-- <div><h1>Welcome to the Exam</h1></div> -->
-    <!-- <button type="button" name="button" class="btn btn-success" id="btnStart"> Start</button> -->
+    <div id="startDiv">
+        <div id="headerDiv">Welcome to the Exam</div> 
+        <div id="bodyDiv" class=" position-relative">
+            <button type="button" name="button" class="btn btn-lg btn-success customBtn position-absolute bottom-0 end-0" id="btnStart"> Start</button>
+        </div>
+    </div>
                 
-    <!-- <div id="questions" class="container" style="width: "></div>           -->
-    <button type="button" name="button" class="btn btn-warning" id="btnFinish">Nộp bài</button>
+    <div id="questions" class="container" style="width: "></div>          
+    <button type="button" name="button" class="btn btn-warning btn-lg position-relative bottom-0 start-50 translate-middle-x" id="btnFinish" style="margin-top: 5%">Nộp bài</button>
 </body>
 <script type="text/javascript">
     $(document).ready(function() {
         $('#btnFinish').hide();
+        $('#questions').hide();
     });
     $('#btnStart').click(function() {
         GetQuestions();
+        $('#questions').show();
         $('#btnFinish').show();
+        $('#startDiv').hide();
+
     });
     var questions;
     let index = 1;
     function sum(a, b){
-    return a + b;
-}
+        return a + b;
+    }
     $('#btnFinish').click(function(){
         $(this).hide();
-        $('#btnStart').show();
+        //$('#btnStart').show();
         CheckResult();
     })
 
@@ -49,28 +60,27 @@
         let mark = 0;
         $('#questions div.row').each(function(k,v) {
             
-            // Bước 1: lấy đáp án đúng của câu hỏi
-            let id = $(v).find('h5').attr('id');
+        // Bước 1: lấy đáp án đúng của câu hỏi
+        let id = $(v).find('h5').attr('id');
     
-            let question = questions.find(x=>x.id == id);
-            let answer = question['ranswer'];
-            //Bước 2: lấy từ người dùng
-            let da = $(v).find('fieldset input[type = "radio"]:checked').attr('class');
-            //da: đáp án của người dùng
-            let choice = '';
-            switch (da) {
-                case 'rdOptionA':
-                    choice = "answer1";
+        let question = questions.find(x=>x.id == id);
+        let answer = question['ranswer'];
+        //Bước 2: lấy từ người dùng
+        let da = $(v).find('fieldset input[type = "radio"]:checked').attr('class');
+        //da: đáp án của người dùng
+        let choice = '';
+        switch (da) {
+            case 'rdOptionA':
+                choice = "answer1";
                 break;
-                case 'rdOptionB':
-                    choice = "answer2";
-                    
+            case 'rdOptionB':
+                choice = "answer2";
                 break;
-                case 'rdOptionC':
-                    choice = "answer3";
+            case 'rdOptionC':
+                choice = "answer3";
                 break;
-                case 'rdOptionD':
-                    choice = "answer4";
+            case 'rdOptionD':
+                choice = "answer4";
                 break;
             }
             if (choice == answer) {
@@ -78,7 +88,6 @@
             } else {
             }
              // Bước 3: đánh dấu câu hỏi nào đúng, câu nào sai
-        
         $('#question_'+id+' >fieldset>div>label.'+answer).innerHTML('<span class="glyphicon glyphicon-ok"></span>');
         });
         console.log('Điểm của bạn là: ' + mark + '/' + sum(index,-1));
@@ -93,29 +102,36 @@
             url: 'renderQ.php',
             type: 'get',
             success: function(data) {
-                questions = jQuery.parseJSON( data);
+                questions = jQuery.parseJSON(data);
                 index = 1;
                 let d  = '';
-                $.each(questions,function(k,v) {         
-            d +=        '<div class="row" style="margin-left:10px;" id = "question_' + v['id']+'"> ';
-            d +=        '<h5 style="font-weight:bold;" id ='+v['id']+'> <span class = "text-danger">Câu ' + index +': </span>' + v['quest'] +'</h5>';
-            if (v['filepath'] != null) d += '<img src = assets/image/' + v['filepath'] + '>';
-
-            d +=         '<fieldset id = "group'+index+'">';
-            d +=        '<div class="radio col-md-12">';
+                $.each(questions,function(k,v) {  
+            d +=        '<div id="aQuestion" class="container">'
+            
+            d +=        '<div class="question" id = "question_' + v['id']+'"> ';
+            d +=        '<h5 id ='+v['id']+'> <span class = "text-danger">Câu ' + index +': </span>' + v['quest'] +'</h5>';
+            d +=        '</div>';            
+            
+            d += '<div class="answer">'
+            if (v['filepath'] != null) 
+                d += '<img src = assets/image/' + v['filepath'] + '>';
+            d +=        '<fieldset id = "group'+index+'">';
+            d +=        '<div class="radio bg-success choice">';
             d +=        '    <label  class = "answer1"><input type="radio" class="rdOptionA" name = "group'+index+'"><span class = "text-danger">A: </span> '+v['answer1']+' </label>';
             d +=        '</div>';
-            d +=        '<div class="radio col-md-12">';
+            d +=        '<div class="radio bg-danger choice">';
             d +=        '    <label class = "answer2"><input type="radio" class="rdOptionB" name = "group'+index+'"><span class = "text-danger">B: </span> '+v['answer2']+'</label>';
             d +=        '</div>';
-            d +=        '<div class="radio col-md-12">';
+            d +=        '<div class="radio bg-light choice">';
             d +=        '    <label class = "answer3"><input type="radio" class="rdOptionC" name = "group'+index+'"><span class = "text-danger">C: </span> '+v['answer3']+'</label>';
             d +=        '</div>';
-            d +=        '<div class="radio col-md-12">';
+            d +=        '<div class="radio bg-info choice">';
             d +=        '    <label class = "answer4"><input type="radio" class="rdOptionD" name = "group'+index+'"><span class = "text-danger">D: </span> '+v['answer4']+'</label>';
             d +=        '</div>';
             d +=         '</fieldset>';
-            d +=        '</div>';
+            d +=        '</div>'
+            
+            d +=        '</div>'
             index++;
 
                 });
@@ -125,6 +141,5 @@
         });
     }
 </script>
-<script src="./assets/js/questions.js"></script>
 </html>
 
